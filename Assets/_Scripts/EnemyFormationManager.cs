1bc9b66ef60bfab04fa52243d4c9ac583123d682
Hatv <trinhhamath@gmail.com>
2024-07-12 18:25:20 +0700

    Add highScorePlayer and refactor code

---
+++ Assets/_Scripts/EnemyFormationManager.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFormationManager : MonoBehaviour
+{
+    private const float enemySpacing = 0.5f;
+    private float enemyHeight;
+
+    public void Initialize(float enemyHeight)
+    {
+        this.enemyHeight = enemyHeight;
+    }
+    public IEnumerator EnemyFormationSequence(List<EnemyController> enemies)
+    {
+        while (!GameManager.Instance.isEndGame)
+        {
+            yield return StartCoroutine(MoveEnemiesToSquareFormation(enemies));
+            yield return new WaitForSeconds(5f);
+            SetDiamondFormation(enemies);
+            yield return new WaitForSeconds(5f);
+            SetTriangleFormation(enemies);
+            yield return new WaitForSeconds(5f);
+            SetRectangleFormation(enemies);
+            yield return new WaitForSeconds(5f);
+        }
+
+    }
+
+
+    IEnumerator MoveEnemiesToSquareFormation(List<EnemyController> enemies)
+    {
+        yield return new WaitForSeconds(1f);
+
+        float screenTop = Camera.main.orthographicSize;
+        float yOffset = screenTop - enemyHeight * 3 / 2;
+
+        // 
+        float formationSize = enemySpacing * 3;
+
+        // 
+        float xCenter = 0f;
+        float xOffset = xCenter - formationSize / 2;
+
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            int row = i / 4;
+            int col = i % 4;
+            Vector3 position = new(col * enemySpacing + xOffset, yOffset - row * enemySpacing, 0);
+            enemies[i].SetTargetPosition(position);
+        }
+    }
+
+    void SetDiamondFormation(List<EnemyController> enemies)
+    {
+        float screenTop = Camera.main.orthographicSize;
+        float yOffset = screenTop - enemyHeight * 3 / 2;
+        float xCenter = 0f;
+        float yCenter = yOffset - 2f * enemySpacing;
+
+        int[] enemiesPerRow = { 1, 4, 6, 4, 1 };
+        int enemyIndex = 0;
+
+        for (int row = 0; row < enemiesPerRow.Length; row++)
+        {
+            int enemiesInThisRow = enemiesPerRow[row];
+            float rowWidth = (enemiesInThisRow - 1) * enemySpacing;
+            float startX = xCenter - rowWidth / 2;
+
+            for (int col = 0; col < enemiesInThisRow; col++)
+            {
+                if (enemyIndex < enemies.Count)
+                {
+                    float x = startX + col * enemySpacing;
+                    float y = yCenter + (2 - row) * enemySpacing;
+                    Vector3 position = new(x, y, 0);
+                    enemies[enemyIndex].SetTargetPosition(position);
+                    enemyIndex++;
+                }
+            }
+        }
+    }
+
+    void SetTriangleFormation(List<EnemyController> enemies)
+    {
+        float screenTop = Camera.main.orthographicSize;
+        float yOffset = screenTop - enemyHeight * 3 / 2;
+        float xCenter = 0f;
+
+        int baseEnemies = 9;
+        float baseWidth = (baseEnemies - 1) * enemySpacing;
+        float startX = xCenter - baseWidth / 2;
+        float height = 4 * enemySpacing; // Chiều cao của tam giác
+
+        // Đặt enemy ở đỉnh tam giác
+        enemies[0].SetTargetPosition(new Vector3(xCenter, yOffset, 0));
+
+        // Đặt enemy ở hai cạnh bên
+        for (int i = 1; i < 4; i++)
+        {
+            float y = yOffset - i * (height / 4);
+            float xOffset = i * (baseWidth / 8);
+
+            enemies[i * 2 - 1].SetTargetPosition(new Vector3(xCenter - xOffset, y, 0));
+            enemies[i * 2].SetTargetPosition(new Vector3(xCenter + xOffset, y, 0));
+        }
+
+        // Đặt enemy ở cạnh đáy
+        for (int i = 0; i < baseEnemies; i++)
+        {
+            float x = startX + i * enemySpacing;
+            enemies[7 + i].SetTargetPosition(new Vector3(x, yOffset - height, 0));
+        }
+    }
+
+    void SetRectangleFormation(List<EnemyController> enemies)
+    {
+        float screenTop = Camera.main.orthographicSize;
+        float yOffset = screenTop - enemyHeight * 3 / 2;
+        float xCenter = 0f;
+
+        int width = 7;
+        int height = 3;
+        float rectangleWidth = (width - 1) * enemySpacing;
+        float rectangleHeight = (height - 1) * enemySpacing;
+
+        float startX = xCenter - rectangleWidth / 2;
+        float startY = yOffset - rectangleHeight - enemySpacing; // Thêm khoảng cách để hạ thấp hình chữ nhật
+
+        int enemyIndex = 0;
+
+        // Đặt enemy ở cạnh trên (7 enemy)
+        for (int i = 0; i < width; i++)
+        {
+            float x = startX + i * enemySpacing;
+            enemies[enemyIndex++].SetTargetPosition(new Vector3(x, startY + rectangleHeight, 0));
+        }
+
+        // Đặt enemy ở cạnh dưới (7 enemy)
+        for (int i = 0; i < width; i++)
+        {
+            float x = startX + i * enemySpacing;
+            enemies[enemyIndex++].SetTargetPosition(new Vector3(x, startY, 0));
+        }
+
+        // Đặt enemy ở hai cạnh bên (1 enemy mỗi bên, không tính các góc)
+        for (int i = 1; i < height - 1; i++)
+        {
+            float y = startY + i * enemySpacing;
+            enemies[enemyIndex++].SetTargetPosition(new Vector3(startX, y, 0));
+            enemies[enemyIndex++].SetTargetPosition(new Vector3(startX + rectangleWidth, y, 0));
+        }
+    }
+
+}
--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -8,20 +8,20 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
-    
 
+    private float enemyHeight;
+    private EnemyFormationManager enemyFormationManager;
+    
     public GameObject enemyPrefab;
     public List<EnemyController> enemies = new();
     private Vector3 offScreenPosition = new(-10f, 6f, 0);
-
-    private const float enemySpacing = 0.5f;
-    private float enemyHeight;
 
     public List<GameObject> img_lives;
     public int score = 0;
     public Text txtScore;
     public Text txtHighScore;
     private int highScore; // Save load qua Playerprefs
+    private string highScorePlayer; //Save load qua Playerprefs
     public Text txtLive;
     public GameObject pnlEndGame;
     public Text txtEndPoint;
@@ -34,8 +34,12 @@ public class GameManager : MonoBehaviour
     public bool isEndGame;
     private bool isStartFirstTime;
     private bool isPaused = false;
+    private bool isHighScoreSaved = false;
 
     [SerializeField] private GameObject pausePanel;
+    [SerializeField] private InputField nameInputField;
+    [SerializeField] private Button saveHighScoreButton;
+    [SerializeField] private Button resetHighScoreButton;
 
     public void PauseGamebutton()
     {
@@ -45,7 +49,6 @@ public class GameManager : MonoBehaviour
             Time.timeScale = 0f;
             isPaused = true;
         }
-        
     }
 
     public void ResumeButton()
@@ -56,7 +59,6 @@ public class GameManager : MonoBehaviour
             Time.timeScale = 1f;
             isPaused = false;
         }
-        
     }
 
     private void Awake()
@@ -72,6 +74,9 @@ public class GameManager : MonoBehaviour
     }
     void Start()
     {
+        enemyFormationManager = gameObject.AddComponent<EnemyFormationManager>();
+        enemyFormationManager.Initialize(enemyHeight);
+
         AudioManager.Instance.PlayBackgroundMusic();
 
         Time.timeScale = 1.0f; //0
@@ -79,16 +84,27 @@ public class GameManager : MonoBehaviour
         pnlEndGame.SetActive(false);
         isStartFirstTime = true;
 
+        //Tinh toan enemyHeight
         GameObject tempEnemy = Instantiate(enemyPrefab, offScreenPosition, Quaternion.identity);
         SpriteRenderer spriteRenderer = tempEnemy.GetComponent<SpriteRenderer>();
         enemyHeight = spriteRenderer.bounds.size.y;
         Destroy(tempEnemy);
 
+        enemyFormationManager = gameObject.AddComponent<EnemyFormationManager>();
+        enemyFormationManager.Initialize(enemyHeight);
+
         SpawnEnemies();
-        StartCoroutine(EnemyFormationSequence());
+        StartCoroutine(enemyFormationManager.EnemyFormationSequence(enemies));
 
         highScore = PlayerPrefs.GetInt("HighScore", 0); // Tai du lieu
-        txtHighScore.text = "High Score: " + highScore.ToString();
+        highScorePlayer = PlayerPrefs.GetString("HighScorePlayer", "None");
+        txtHighScore.text = $"High Score:\n {highScorePlayer} - {highScore}";
+
+        saveHighScoreButton.onClick.AddListener(SaveHighScore);
+        //resetHighScoreButton.onClick.AddListener(ResetHighScore);
+        nameInputField.gameObject.SetActive(false);
+        saveHighScoreButton.gameObject.SetActive(false);
+        resetHighScoreButton.gameObject.SetActive(true);
     }
 
     private void Update()
@@ -121,159 +137,10 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    IEnumerator EnemyFormationSequence()
-    {
-        while (true)
-        {
-            yield return StartCoroutine(MoveEnemiesToSquareFormation());
-            yield return new WaitForSeconds(5f);
-            SetDiamondFormation();
-            yield return new WaitForSeconds(5f);
-            SetTriangleFormation();
-            yield return new WaitForSeconds(5f);
-            SetRectangleFormation();
-            yield return new WaitForSeconds(5f);
-        }
-
-    }
-
-    IEnumerator MoveEnemiesToSquareFormation()
-    {
-        yield return new WaitForSeconds(1f);
-
-        float screenTop = Camera.main.orthographicSize;
-        float yOffset = screenTop - enemyHeight * 3 / 2;
-
-        // 
-        float formationSize = enemySpacing * 3;
-
-        // 
-        float xCenter = 0f;
-        float xOffset = xCenter - formationSize / 2;
-
-        for (int i = 0; i < enemies.Count; i++)
-        {
-            int row = i / 4;
-            int col = i % 4;
-            Vector3 position = new(col * enemySpacing + xOffset, yOffset - row * enemySpacing, 0);
-            enemies[i].SetTargetPosition(position);
-        }
-    }
-
-    void SetDiamondFormation()
-    {
-        float screenTop = Camera.main.orthographicSize;
-        float yOffset = screenTop - enemyHeight * 3 / 2;
-        float xCenter = 0f;
-        float yCenter = yOffset - 2f * enemySpacing;
-
-        int[] enemiesPerRow = { 1, 4, 6, 4, 1 };
-        int enemyIndex = 0;
-
-        for (int row = 0; row < enemiesPerRow.Length; row++)
-        {
-            int enemiesInThisRow = enemiesPerRow[row];
-            float rowWidth = (enemiesInThisRow - 1) * enemySpacing;
-            float startX = xCenter - rowWidth / 2;
-
-            for (int col = 0; col < enemiesInThisRow; col++)
-            {
-                if (enemyIndex < enemies.Count)
-                {
-                    float x = startX + col * enemySpacing;
-                    float y = yCenter + (2 - row) * enemySpacing;
-                    Vector3 position = new(x, y, 0);
-                    enemies[enemyIndex].SetTargetPosition(position);
-                    enemyIndex++;
-                }
-            }
-        }
-    }
-
-    void SetTriangleFormation()
-    {
-        float screenTop = Camera.main.orthographicSize;
-        float yOffset = screenTop - enemyHeight * 3 / 2;
-        float xCenter = 0f;
-
-        int baseEnemies = 9;
-        float baseWidth = (baseEnemies - 1) * enemySpacing;
-        float startX = xCenter - baseWidth / 2;
-        float height = 4 * enemySpacing; // Chiều cao của tam giác
-
-        // Đặt enemy ở đỉnh tam giác
-        enemies[0].SetTargetPosition(new Vector3(xCenter, yOffset, 0));
-
-        // Đặt enemy ở hai cạnh bên
-        for (int i = 1; i < 4; i++)
-        {
-            float y = yOffset - i * (height / 4);
-            float xOffset = i * (baseWidth / 8);
-
-            enemies[i * 2 - 1].SetTargetPosition(new Vector3(xCenter - xOffset, y, 0));
-            enemies[i * 2].SetTargetPosition(new Vector3(xCenter + xOffset, y, 0));
-        }
-
-        // Đặt enemy ở cạnh đáy
-        for (int i = 0; i < baseEnemies; i++)
-        {
-            float x = startX + i * enemySpacing;
-            enemies[7 + i].SetTargetPosition(new Vector3(x, yOffset - height, 0));
-        }
-    }
-
-    void SetRectangleFormation()
-    {
-        float screenTop = Camera.main.orthographicSize;
-        float yOffset = screenTop - enemyHeight * 3 / 2;
-        float xCenter = 0f;
-
-        int width = 7;
-        int height = 3;
-        float rectangleWidth = (width - 1) * enemySpacing;
-        float rectangleHeight = (height - 1) * enemySpacing;
-
-        float startX = xCenter - rectangleWidth / 2;
-        float startY = yOffset - rectangleHeight - enemySpacing; // Thêm khoảng cách để hạ thấp hình chữ nhật
-
-        int enemyIndex = 0;
-
-        // Đặt enemy ở cạnh trên (7 enemy)
-        for (int i = 0; i < width; i++)
-        {
-            float x = startX + i * enemySpacing;
-            enemies[enemyIndex++].SetTargetPosition(new Vector3(x, startY + rectangleHeight, 0));
-        }
-
-        // Đặt enemy ở cạnh dưới (7 enemy)
-        for (int i = 0; i < width; i++)
-        {
-            float x = startX + i * enemySpacing;
-            enemies[enemyIndex++].SetTargetPosition(new Vector3(x, startY, 0));
-        }
-
-        // Đặt enemy ở hai cạnh bên (1 enemy mỗi bên, không tính các góc)
-        for (int i = 1; i < height - 1; i++)
-        {
-            float y = startY + i * enemySpacing;
-            enemies[enemyIndex++].SetTargetPosition(new Vector3(startX, y, 0));
-            enemies[enemyIndex++].SetTargetPosition(new Vector3(startX + rectangleWidth, y, 0));
-        }
-    }
-
     public void AddScore(int points)
     {
         score += points;
         txtScore.text = "Score\n" + score.ToString();
-        //Debug.Log("Score: " + score);
-
-        if (score > highScore)
-        {
-            highScore = score;
-            txtHighScore.text = "High Score: " + highScore.ToString();
-            PlayerPrefs.SetInt("HighScore", highScore); //luu du lieu
-            PlayerPrefs.Save();
-        }
     }
 
     public void UpdateLives(int lives)
@@ -297,9 +164,15 @@ public class GameManager : MonoBehaviour
 
     public void ReStart()
     {
-        //Debug.Log("Test Restart");
-        StartGane();
-        AudioManager.Instance.ResetMusic();
+        if (score >= highScore && nameInputField.gameObject.activeSelf && !isHighScoreSaved)
+        {
+            txtEndPoint.text = "Please enter your name to save the score before restarting.";
+        }
+        else
+        {
+            StartGane();
+            AudioManager.Instance.ResetMusic();
+        }
     }
 
     public void RestartBottonClick()
@@ -330,28 +203,76 @@ public class GameManager : MonoBehaviour
         //pausePanel.SetActive(false);
         txtEndPoint.text = "Your Score:\n" + score.ToString();
 
-        if (score > highScore)
+        if (score >= highScore)
         {
             highScore = score;
             PlayerPrefs.SetInt("HighScore", highScore); // Luu du lieu
             PlayerPrefs.Save();
-        }
 
-        txtHighScore.text = "High Score: " + highScore.ToString();
+            resetHighScoreButton.gameObject.SetActive(false);
+            nameInputField.gameObject.SetActive(true);
+            saveHighScoreButton.gameObject.SetActive(true);
+            txtEndPoint.text = "Congratulations! You've got a new high score. Please enter your name and save.";
+        }
+        else
+        {
+            resetHighScoreButton.gameObject.SetActive(true);
+            nameInputField.gameObject.SetActive(false);
+            saveHighScoreButton.gameObject.SetActive(false);
+            txtHighScore.text = $"High Score:\n {highScorePlayer} - {highScore}";
+        }
+    }
+
+    public void SaveHighScore()
+    {
+        if (score >= highScore)
+        {
+            string playerName = nameInputField.text;
+            if (!string.IsNullOrEmpty(playerName))
+            {
+                highScorePlayer = playerName;
+                PlayerPrefs.SetString("HighScorePlayer", highScorePlayer);
+                PlayerPrefs.SetInt("HighScore", highScore);
+                PlayerPrefs.Save();
+                txtHighScore.text = $"High Score:\n {highScorePlayer} - {highScore}";
+
+                isHighScoreSaved = true;
+
+                nameInputField.gameObject.SetActive(false);
+                saveHighScoreButton.gameObject .SetActive(false);
+                
+            }
+            else
+            {
+                txtEndPoint.text = "Please enter your name to save the score.";
+            }
+        }
+        else
+        {
+            Debug.Log("Your score is not high enough to save.");
+        }    
     }
 
     public void ResetHighScore()
     {
         PlayerPrefs.DeleteKey("HighScore");
+        PlayerPrefs.DeleteKey("HighScorePlayer");
         highScore = 0;
-        txtHighScore.text = "High Score: " + highScore.ToString();
+        highScorePlayer = "None";
+        txtHighScore.text = $"High Score:\n {highScorePlayer} - {highScore}";
     }
     
     public void OptionsButton()
     {
-        AudioManager.Instance.StopAllSounds();
-        SceneManager.LoadScene("MainMenu");
-        
+        if (score >= highScore && nameInputField.gameObject.activeSelf && !isHighScoreSaved)
+        {
+            txtEndPoint.text = "Please enter your name to save the score before going to options.";
+        }
+        else
+        {
+            AudioManager.Instance.StopAllSounds();
+            SceneManager.LoadScene("MainMenu");
+        }
     }
 
     public void QuitGameButton()
